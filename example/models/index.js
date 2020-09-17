183db569026b880820fb53b69120bceab656d90a
jade570 <chaelyoung@naver.com>
2020-09-17 10:50:10 +0900

    Add Sequelize

---
+++ example/models/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+
+let db = {};
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = require('./user')(sequeilze, Sequelize);
+
+module.exports = db;
